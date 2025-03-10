ae8b26639172583cb729c52c629c3be3858aafcf
ivanr <ivanroaprieto@gmail.com>
2025-03-10 19:07:32 +0100

    information of the day in every horoscope

--- app/src/main/java/com/example/zodiacsign/DetailActivity.kt
+++ app/src/main/java/com/example/zodiacsign/DetailActivity.kt
@@ -11,6 +11,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class DetailActivity : AppCompatActivity() {
 
@@ -21,6 +29,8 @@ class DetailActivity : AppCompatActivity() {
     lateinit var nameTextView: TextView
     lateinit var dateTextView: TextView
     lateinit var iconImageView: ImageView
+    lateinit var horoscopeLuckTextView: TextView
+
 
     lateinit var horoscope: Horoscope
     var isFavorite = false
@@ -92,6 +102,9 @@ class DetailActivity : AppCompatActivity() {
         iconImageView.setImageResource(horoscope.icon)
 
         isFavorite = session.isFavorite(horoscope.id)
+
+        getHoroscopeLuck()
+
     }
 
     private fun initView() {
@@ -100,12 +113,51 @@ class DetailActivity : AppCompatActivity() {
         nameTextView = findViewById(R.id.nameTextView)
         dateTextView = findViewById(R.id.dateTextView)
         iconImageView = findViewById(R.id.iconImageView)
+        horoscopeLuckTextView = findViewById(R.id.horoscopeLuckTextView)
+
     }
     private fun setFavoriteIcon() {
         if (isFavorite) {
             favoriteMenu.setIcon(R.drawable.ic_favorite_selected)
         } else {
             favoriteMenu.setIcon(R.drawable.ic_favorite)
+        }
+    }
+
+    private fun getHoroscopeLuck () {
+        CoroutineScope(Dispatchers.IO).launch {
+            var urlConnection: HttpsURLConnection? = null
+
+            try {
+                val url = URL("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${horoscope.id}&day=TODAY")
+                urlConnection = url.openConnection() as HttpsURLConnection
+
+                if (urlConnection.responseCode == 200) {
+                    val rd = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    var line: String?
+                    val stringBuilder = StringBuilder()
+                    while ((rd.readLine().also { line = it }) != null) {
+                        stringBuilder.append(line)
+                    }
+                    val result = stringBuilder.toString()
+
+                    // Instantiate a JSON object from the request response
+                    val jsonObject = JSONObject(result)
+                    val horoscopeLuck = jsonObject.getJSONObject("data").getString("horoscope_data")
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        horoscopeLuckTextView.text = horoscopeLuck
+                    }
+                    /*runOnUiThread {
+
+                    }*/
+                    //Log.i("HTTP", horoscopeLuck)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                urlConnection?.disconnect()
+            }
         }
     }
 }
